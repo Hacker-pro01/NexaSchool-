0a9a718c5297a9e55a73f0f968adca52b8ccbf9c
HackSam01 <hacksamviery@gmail.com>
2025-12-04 21:38:05 +0100

    Create reusable Input component for NexaSchool application

---
+++ src/components/common/Input.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ type, placeholder, value, onChange, className }) => {
+    return (
+        <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className={className}
+            style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}
+        />
+    );
+};
+
+Input.propTypes = {
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    className: PropTypes.string,
+};
+
+Input.defaultProps = {
+    type: 'text',
+    placeholder: '',
+    value: '',
+    className: '',
+};
+
+export default Input;
